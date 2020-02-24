8f1432d35538591b1f35d11c1e3ddd480f299586
JohnYehyo <836524969@qq.com>
2020-02-24 23:08:07 +0800

    函数中变量的作用域

--- 05函数.py
+++ 05函数.py
@@ -56,13 +56,57 @@
 #     print(k, '=', v)
 
 
-'''
+"""
 模块中的函数除了定义函数之外还有可以执行的代码,那么Python解释器会直接执行这些代码
 如果我们在模块中编写了执行代码，最好是将这些执行代码放入如下所示的条件中，这样的话
 除非直接运行该模块，if条件下的这些代码是不会执行的，因为只有直接执行的模块的名字才
 是“__main__”。
-'''
+"""
 
 # 导入module1时 不会执行模块中if条件成立时的代码 因为模块的名字是module3而不是__main__
 import module1
 
+
+# 函数中变量作用域问题
+# 1.
+# def foo():
+#     b = 'hello'
+#
+#     def bar():  # Python中可以在函数内部再定义函数
+#         c = True
+#         print(a)
+#         print(b)
+#         print(c)
+#
+#     bar()
+#     # print(c)  # NameError: name 'c' is not defined
+#
+#
+# if __name__ == '__main__':
+#     a = 100
+#     # print(b)  # NameError: name 'b' is not defined
+#     foo()
+
+# 2.
+# def foo():
+#     a = 200
+#     print(a)  # 200
+#
+#
+# if __name__ == '__main__':
+#     a = 100
+#     foo()
+#     print(a)  # 100
+
+# 这里foo中是声明了一个a=200的局部变量, print(a) 也不会获取到他而是使用自己的局部变量a还为100,想要改变a的值输出200 可以设置全局变量
+
+# def foo():
+#     global a
+#     a = 200
+#     print(a)
+#
+#
+# if __name__ == '__main__':
+#     a = 100
+#     foo()
+#     print(a)
