32c2adf022f9f2cadbb4184cdde5e82cfd0a26c1
JohnYehyo <836524969@qq.com>
2020-02-27 22:31:57 +0800

    继承和多态

--- 07继承和多态.py
+++ 07继承和多态.py
@@ -2,7 +2,7 @@ from Cat import Cat
 from Dog import Dog
 
 
-def jicheng():
+def do():
     pets = [Dog('狗狗', '小黑'), Cat('猫猫', '小花')]
     for p in pets:
         # 继承
@@ -13,4 +13,4 @@ def jicheng():
 
 
 if __name__ == '__main__':
-    jicheng()
+    do()
