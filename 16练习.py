74673726d3f4c24d1ce4d971c9040205092d76ec
JohnYehyo <836524969@qq.com>
2020-03-24 16:25:48 +0800

    练习

--- 16练习.py
+++ 16练习.py
@@ -1,12 +1,13 @@
 import os
+import random
 import time
 
 
 def reveal():
-    '''
-        跑马灯效果
+    """
+    跑马灯效果
     :return:
-    '''
+    """
     content = '你看到我了'
     while True:
         # windows
@@ -18,5 +19,24 @@ def reveal():
         content = content[1:] + content[0]
 
 
+def randomStr(length):
+    """
+    产生指定长度随机码
+    :param length:
+    :return:
+    """
+    # 指定一个码池
+    strPool = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+               'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+               'y']
+    mark = ''
+    poolLength = len(strPool) - 1
+    for i in range(length):
+        index = random.randint(0, poolLength)
+        mark += strPool[index]
+    return mark
+
+
 if __name__ == '__main__':
-    reveal()
+    # reveal()
+    print(randomStr(6))
